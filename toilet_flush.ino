83c2fbc06a30be2ff906139cf02113dcad22aa34
masakazu matsubara <matsu.boolean@gmail.com>
2022-08-11 12:50:08 +0900

    アニメーションの追加・外部/内部赤外線LED両対応

--- toilet_flush.ino
+++ toilet_flush.ino
@@ -12,6 +12,7 @@
  * * M5StickC Plus
  * * M5Stack用赤外線送受信ユニット [U002] (本体の赤外線LEDが使える場合は不要・送信コマンドを赤外線センサで解析する必要がある場合は必要)
  * * M5StickC ToF Hat（必須ではない。使用した場合は、M5ボタンの代わりに距離センサーで操作可能)
+ * * M5StickC PIR Hat（必須ではない。使用した場合は、接近するとディスプレイをONにできる)
  * 
  * ## 参考
  *   M5StickC(ESP32)で赤外線リモコンを作ろう
@@ -50,8 +51,8 @@
 //#define DEBUG
 
 // 赤外線LED接続端子定数
-const uint16_t IR_LED = 9;  // 内蔵赤外線 LED
-//const uint16_t IR_LED = 32; // M5Stack用赤外線送受信ユニット(GROVE互換端子)
+const uint16_t IR_LED_INTERNAL = 9;  // 内蔵赤外線 LED
+const uint16_t IR_LED_EXTERNAL = 32; // M5Stack用赤外線送受信ユニット(GROVE互換端子)
 
 // PIR HAT 接続端子定数
 const uint16_t PIR = 36;  // 人感センサー
@@ -65,39 +66,50 @@ const int SITDOWN_TIMER   = 60000; // 長時間着座タイマー(ms)
 const int COUNTDOWN_TIMER = 120000; // カウントダウンタイマー(ms)（離席後時間経過後にトイレフラッシュ）
 #endif
 
-const int DISPLAY_ON_TIMER = COUNTDOWN_TIMER; // 人感センサー検知後のディスプレイ点灯時間(ms)
-
+// 人感センサー検知後のディスプレイ点灯時間(ms)
+const int DISPLAY_TIMER = COUNTDOWN_TIMER;
 
 // 処理ごとの待ち時間(ms)
 const int DELAY_TIME = 30;
+
+// 赤外線送信クラス
+IRsend irsendExternal(IR_LED_EXTERNAL); // M5Stack用赤外線送受信ユニット(GROVE互換端子)
+IRsend irsendInternal(IR_LED_INTERNAL); // 内蔵赤外線 LED
+
+// 距離計(ToFセンサー)
+VL53L0X rangefinder;
+
+// loop処理の時刻（loop()関数の中で更新）
+uint32_t timeValue = millis();
 
 // ステータス
 enum class Status { Waiting, SitOn, SitOnLong, Countdown, ManualCountdown };
 Status status = Status::Waiting;
 // 一部のステータスで使用するステータスが変更された時刻
-unsigned long timeChangeStatus = 0;
+uint32_t timeChangeStatus = 0;
 
 // ディスプレイを点灯した時刻
-unsigned long timeDisplayOn = 0;
+uint32_t timeDisplayOn = 0;
 
-// 赤外線送信クラス
-IRsend irsend(IR_LED);  // Set the GPIO to be used to sending the message.
-
-// 距離計(ToFセンサー)
-VL53L0X rangefinder;
-// 距離計(ToFセンサー)を利用するか
+// 距離計(ToF HAT)を利用するか
 boolean rangefinderUseFlag = false;
 
-// 人感センサー(PIR HAT)
+// 人感センサー(PIR HAT)を利用するか
 boolean pirUseFlag = false;
 
 // 画面利用フラグ
-boolean lcdUseFlag = false; // デフォルト消灯
+boolean displayOnFlag = false; // デフォルト消灯
+
+// アニメーションカウンター
+int animeCounter = 0;
+// アニメーションタイマー
+uint32_t timeAnime = 0;
+
 
 // ステータス変更
 void changeStatus(Status newStatus) {
   status = newStatus;
-  timeChangeStatus = millis();
+  timeChangeStatus = timeValue;
   if (status != Status::Waiting)
     displayOn();
 }
@@ -107,35 +119,94 @@ void changeStatus(Status newStatus) {
  */
 void initDisplay() {
   // 解像度：135x240
-  //M5.Lcd.setRotation(3);
+  //M5.Lcd.setRotation(2);
   M5.Lcd.fillScreen(BLACK);
+
+  M5.Lcd.fillRect(0, 0, 135, 18, NAVY);
+  M5.Lcd.setTextColor(WHITE, NAVY);
   M5.Lcd.setCursor(5, 0, 2);
   M5.Lcd.println("Toilet flush v0.3");
+
+  M5.Lcd.setTextColor(WHITE, BLACK);
+}
+
+/**
+ * アニメーションのキャラクタ表示
+ */
+void drawAnime() {
+    int offsetX = 0;
+//    if (animeCounter % 10 == 3 || animeCounter % 10 == 5 || animeCounter % 10 == 6)
+//      offsetX = 3;
+//    if (animeCounter % 10 == 9)
+//      offsetX = -7;
+    int16_t edgeColor = NAVY;
+    int16_t outColor = BLUE;
+    int16_t inColor = CYAN;
+    if (rangefinderUseFlag) {
+      edgeColor = RED;
+      outColor = ORANGE;
+      inColor = YELLOW;
+    }
+    if (animeCounter % 2 == 0) {
+      M5.Lcd.fillCircle(66, 160, 33, BLACK);
+      M5.Lcd.fillCircle(66, 160, 30, edgeColor);
+      M5.Lcd.fillCircle(66, 160, 29, outColor);
+      M5.Lcd.fillCircle(66 + offsetX, 160, 15, inColor);
+    } else {
+      M5.Lcd.fillCircle(66, 160, 33, edgeColor);
+      M5.Lcd.fillCircle(66, 160, 32, outColor);
+      M5.Lcd.fillCircle(66 + offsetX, 160, 12, inColor);
+    }
+    animeCounter++;
+}
+
+/**
+ * スプラッシュ画面
+ */
+void displaySplash() {
+  initDisplay();
+
+  M5.Lcd.setCursor(5, 20, 4);
+  M5.Lcd.println("Welcome");
+  M5.Lcd.println("          to");
+  M5.Lcd.println("        toilet");
+
+  for (int idx = 0; idx < 4; idx++) {
+    drawAnime();
+    delay(1000);
+  }
+  animeCounter = 0;
 }
 
 /**
  * ディスプレイON
  */
 void displayOn() {
-  lcdUseFlag = true;
-  M5.Axp.SetLDO2(lcdUseFlag);
+  displayOnFlag = true;
+  M5.Axp.SetLDO2(displayOnFlag);
   timeDisplayOn = millis();
+
+  // CPU速度を240Mhzに変更
+  setCpuFrequencyMhz(240);
 }
 
 /**
  * ディスプレイOFF
  */
 void displayOff() {
-  lcdUseFlag = false;
-  M5.Axp.SetLDO2(lcdUseFlag);
+  displayOnFlag = false;
+  M5.Axp.SetLDO2(displayOnFlag);
+
+  // CPU速度を10Mhzに変更
+  setCpuFrequencyMhz(10);
 }
 
 /**
  * ディスプレイON/OFFトグル
  */
 void displayToggle() {
-  lcdUseFlag = !lcdUseFlag;
-  M5.Axp.SetLDO2(lcdUseFlag);
+  displayOnFlag = !displayOnFlag;
+  M5.Axp.SetLDO2(displayOnFlag);
 }
 
 /**
@@ -146,11 +217,20 @@ void flush() {
   // CPU の速度が10Mhzのままだと赤外線が送れない
   setCpuFrequencyMhz(240);
   M5.Lcd.fillScreen(BLUE);
-  irsend.sendInax(0x5C30CF);
+  M5.Lcd.setTextColor(WHITE, BLUE);
+  M5.Lcd.setCursor(20, 127, 4);
+  M5.Lcd.println("FLUSH !!");
+  M5.Lcd.setTextColor(WHITE, BLACK);
+  
+  irsendExternal.sendInax(0x5C30CF);
+  delay(500);
+  irsendInternal.sendInax(0x5C30CF);
   delay(500);
   initDisplay();
-  // CPU速度を10Mhzに変更
-  setCpuFrequencyMhz(10);
+
+  // CPU速度を戻す
+  if (displayOnFlag == false)
+    setCpuFrequencyMhz(10);
 }
 
 
@@ -159,14 +239,14 @@ void setup() {
   M5.begin();
   
   // LCD明るさ
-  M5.Axp.ScreenBreath(10); // 6より下はかなり見づらく、消費電力もあまり落ちないらしい
-  M5.Axp.SetLDO2(lcdUseFlag);
+  M5.Axp.ScreenBreath(12); // 6より下はかなり見づらく、消費電力もあまり落ちないらしい
+  M5.Lcd.fillScreen(BLACK);
 
-  // ディスプレイ初期化
-  initDisplay();
-  
-  // 赤外線LEDの初期化
-  irsend.begin();
+  // 外付け赤外線LEDの初期化
+  irsendExternal.begin();
+
+  // 内蔵赤外線LEDの初期化
+  irsendInternal.begin();
 
   // 距離計の初期化
   Wire.begin(0, 26, 100000UL); // I2C of HAT Connection
@@ -185,12 +265,23 @@ void setup() {
 
   // 6軸センサ初期化
   M5.Imu.Init();
+
+  changeStatus(Status::Waiting);
+
+  // ディスプレイ初期化
+  displayOn();
+  displaySplash();
+  initDisplay();
+  displayOff();
   
   // CPUスピードを10MHzに変更
   setCpuFrequencyMhz(10);
 }
 
 void loop() {
+  // 処理時刻の更新
+  timeValue = millis();
+
   M5.update();
 
   // 加速度取得
@@ -207,6 +298,8 @@ void loop() {
   // ボタン値取得
   boolean btnA = M5.BtnA.wasReleased();
   boolean btnB = M5.BtnB.wasReleased();
+  boolean btnPower = (M5.Axp.GetBtnPress() != 0);
+
   // 距離計をボタンAと同じ扱いにする(本体を立てた状態かつ20cm以下で押下扱い)
   if (rangefinderUseFlag) {
     uint16_t distance = rangefinder.readRangeContinuousMillimeters();
@@ -231,8 +324,8 @@ void loop() {
   }
 
   // ディスプレイ消灯判定
-  if (lcdUseFlag) {
-    if (millis() - timeDisplayOn > DISPLAY_ON_TIMER) {
+  if (displayOnFlag && status == Status::Waiting) {
+    if (timeValue - timeDisplayOn > DISPLAY_TIMER) {
       displayOff();
     }
   }
@@ -240,7 +333,7 @@ void loop() {
   // ボタン処理
   if (btnA) {
     // A ボタンが押されたらディスプレイ点灯・トイレフラッシュ・トイレフラッシュキャンセル
-    if (lcdUseFlag == false) {
+    if (displayOnFlag == false) {
       displayOn();
     } else if (status != Status::Countdown && status != Status::ManualCountdown) {
       changeStatus(Status::ManualCountdown); // 手動カウントダウン 
@@ -255,7 +348,7 @@ void loop() {
     flush();
     changeStatus(Status::Waiting);
   }
-  if (M5.Axp.GetBtnPress() != 0) {
+  if (btnPower) {
     // 電源ボタンを押すと（6秒未満）リセット
     esp_restart();
   }
@@ -270,7 +363,7 @@ void loop() {
   case Status::SitOn:   // 着座確認（長時間着座待ち）
     if (sitOnFlg == false) {
       changeStatus(Status::Waiting);
-    } else if (millis() - timeChangeStatus >= SITDOWN_TIMER) {
+    } else if (timeValue - timeChangeStatus >= SITDOWN_TIMER) {
       changeStatus(Status::SitOnLong);
     }
     break;
@@ -280,15 +373,23 @@ void loop() {
     }
     break;
   case Status::Countdown:   // カウントダウン
+    Serial.print("timeValue: ");
+    Serial.print(timeValue);
+    Serial.print(", timeChangeStatus: ");
+    Serial.println(timeChangeStatus);
     if (sitOnFlg) {
       changeStatus(Status::SitOnLong); // 着座した(長時間着座(離席待ち)に戻る)
-    } else if (millis() - timeChangeStatus >= COUNTDOWN_TIMER) {
+    } else if (timeValue - timeChangeStatus >= COUNTDOWN_TIMER) {
       flush();
       changeStatus(Status::Waiting);
     }
     break;
   case Status::ManualCountdown:   // 手動カウントダウン
-    if (millis() - timeChangeStatus >= COUNTDOWN_TIMER) {
+    Serial.print("timeValue: ");
+    Serial.print(timeValue);
+    Serial.print(", timeChangeStatus: ");
+    Serial.println(timeChangeStatus);
+    if (timeValue - timeChangeStatus >= COUNTDOWN_TIMER) {
       flush();
       changeStatus(Status::Waiting);
     }
@@ -315,19 +416,28 @@ void loop() {
     break;
   }
 
+  // カウントダウンタイマー表示
   M5.Lcd.setCursor(20, 60, 7);
   if (status == Status::Countdown || status == Status::ManualCountdown) {
-    int secTime = (COUNTDOWN_TIMER - (millis() - timeChangeStatus)) / 1000;
+    int secTime = (COUNTDOWN_TIMER - (timeValue - timeChangeStatus)) / 1000;
     if (secTime < 0)
       secTime = 0;  // タイミングによってはマイナスになってしまうこともある
-    M5.Lcd.printf("%.03ds   ", secTime);
+    M5.Lcd.printf("%.03d", secTime);
   } else {
     M5.Lcd.print("        ");
   }
 
+  // アニメーション
+  if (timeValue - timeAnime >= 1000) {
+    drawAnime();
+    timeAnime = timeValue;
+  }
+
   // 加速度のデバッグ表示
+  M5.Lcd.setTextColor(LIGHTGREY, BLACK);
   M5.Lcd.setCursor(5, 208, 2);
-  M5.Lcd.printf("Acc:\n  %+.2f %+.2f %+.2f   ", accX, accY, accZ);
+  M5.Lcd.printf("Posture:\n  %+.2f %+.2f %+.2f   ", accX, accY, accZ);
+  M5.Lcd.setTextColor(WHITE, BLACK);
 
   delay(DELAY_TIME);
 }
